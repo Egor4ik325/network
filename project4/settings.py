5f1ea964a14b01f0abdec804ed89e56dfd6e43b1
Egor4ik325 <zorinegor325@gmail.com>
2021-10-04 18:46:47 +0300

    Setup continuous delivery to AWS VPS

--- project4/settings.py
+++ project4/settings.py
@@ -15,11 +15,11 @@ from environ import Env
 from django.core.management.utils import get_random_secret_key
 
 env = Env()
-Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
